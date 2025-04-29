23f4d8d93173420dc482a87c745a66b9ab6e1474
Marek Kopciuszynski <mkopciuszynski@gmail.com>
2025-04-29 10:10:13 +0200

    Use _BV convention and recalibrate OCR0A value to get 10Hz freq

--- at13_low_power_24h_timer/main_10Hz_timer.c
+++ at13_low_power_24h_timer/main_10Hz_timer.c
@@ -8,19 +8,18 @@ volatile uint16_t timer_ticks = 0;
 
 int main(void) {
     cli();
+
     // Set Timer0 to CTC mode
-    TCCR0A |= (1 << WGM01); // CTC mode
-
+    TCCR0A |= _BV(WGM01); // CTC mode
     // Set prescaler to 256
-    TCCR0B |= (1 << CS02);
-
-    // Calculate and set the OCR0A value for 20 Hz
-    // Formula: OCR0A = (F_CPU / (2 * Prescaler * Target Frequency)) - 1
-    // Adjust to get exactly 20 Hz (use oscilloscope)
-    OCR0A = 120;
-
+    TCCR0B |= _BV(CS02);
+    // Adjustest to get 10 Hz
+    OCR0A = 240;
     // Enable Timer0 Compare Match A interrupt
-    TIMSK0 |= (1 << OCIE0A);
+    TIMSK0 |= _BV(OCIE0A);
+
+    // START
+    TCNT0 = 0;
     sei();
 
     DDRB = _BV(OUT_PN); // Set PB4 as output
