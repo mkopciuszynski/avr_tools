47c0f41d9f46489a79d9ec6f29376d750da99367
Marek Kopciuszynski <mkopciuszynski@gmail.com>
2025-04-29 10:09:33 +0200

    🧹 Use OUT_PN instead of LED_PN

--- at13_low_power_24h_timer/main_1s_blink.c
+++ at13_low_power_24h_timer/main_1s_blink.c
@@ -1,10 +1,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED_PN         PB4
+#define OUT_PN         PB4
 
 int main(void) {
     DDRB = _BV(OUT_PN); // Set PB4 as output
+    
     while (1) {
         PORTB ^= _BV(OUT_PN); // Toggle PB4
         _delay_ms(1000);   // Delay for 1 second
