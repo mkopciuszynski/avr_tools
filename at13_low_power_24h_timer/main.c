915dcf08847689c360018cbafd0bb0cb1d3f29db
Marek Kopciuszynski <mkopciuszynski@gmail.com>
2025-04-29 10:34:10 +0200

    💥 Fix important issue. Use while loop with flag instead of delay
    Interrupts inside _delay_ms() don't cancel it, they pause the CPU shortly, then it continues the delay counting.

--- at13_low_power_24h_timer/main.c
+++ at13_low_power_24h_timer/main.c
@@ -2,69 +2,77 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define OUT_PN  PB4
+#define OUT_PN         PB4
+#define OUT_ON         PORTB |=  _BV(OUT_PN)
+#define OUT_OFF        PORTB &= ~_BV(OUT_PN)
+#define SEC_IN_QUARTER  900
 
-
-volatile uint8_t wdt8_tc = 0;
-volatile uint16_t ticks_for_quarter = 0;
+volatile uint16_t wdt8_tc, ticks_for_quarter;
+volatile uint8_t wdt_lock_flag;
+uint8_t i, q;
 
 int main(void) {
-    
     cli();
-    // enable 8 second WDT
+
+    // Enable 8-second WDT
     WDTCR = _BV(WDCE) | _BV(WDE);
     WDTCR = _BV(WDE) | _BV(WDP0) | _BV(WDP3);
     sei();
-    //
-    asm("wdr");
 
+    asm("wdr");
     DDRB = _BV(OUT_PN); // Set PB4 as output
-    //////////////////////////////
 
     while (1) {
-        // toggle every 15 minutes
-        asm("wdr");
 
-        // kalibracja czasu
-        // Set Timer0 to CTC mode
-        TCCR0A |= _BV(WGM01); // CTC mode
-        // Set prescaler to 256
-        TCCR0B |= _BV(CS02);
-        // Adjustest to get 20 Hz
-        OCR0A = 120;
-        // Enable Timer0 Compare Match A interrupt
-        TIMSK0 |= _BV(OCIE0A);
+        // repeat 24*4 times to get 24 hours
+        for (q = 0; q < 96; ++q) {
 
-        // WDT as interrupt
-        WDTCR |= _BV(WDTIE);
-        // START
-        wdt8_tc = 0;
-        TCNT0 = 0;
-        asm("wdr");
-        // lock to trigger WDT interrupt
-        _delay_ms(10000);
-        // now wdt8_tc is around 100 
-        asm("wdr");
-        uint8_t i;
-        for (i=0; i<ticks_for_quarter; ++i){
-            MCUCR = _BV(SM1) | _BV(SE);  //power-down | sleep enable
-            WDTCR |= _BV(WDTIE);         // WDT as interrupt
-            ///////////////
-            asm("sleep");
-            // wakeup
+            // Calibrate the WDT timmer
             asm("wdr");
-        }
-        PORTB ^=_BV(PB4);
-    }
-    return (0);
-}
+            WDTCR |= _BV(WDTIE); // WDT as interrupt
 
+            // Adjust to get 10 Hz blink frequency
+            TCNT0 = 0;
+            OCR0A = 240;
+
+            // Set Timer0 to CTC mode
+            TCCR0A |= _BV(WGM01);  // CTC mode
+            TCCR0B |= _BV(CS02);   // Set prescaler to 256
+            TIMSK0 |= _BV(OCIE0A); // Enable Timer0 CMA trigger
+
+            wdt_lock_flag = 1;
+            wdt8_tc = 0;
+            asm("wdr");
+            // Lock to trigger WDT interrupt
+            while (wdt_lock_flag)
+
+            // Now wdt8_tc is around 100 for 15 minutes
+            // sec_in_quarter * frequency / wdt8_tc - 1
+            ticks_for_quarter = (SEC_IN_QUARTER * 20) / wdt8_tc - 1;
+
+            for (i = 0; i < ticks_for_quarter; ++i) {
+                MCUCR = _BV(SM1) | _BV(SE);  // Power-down | sleep enable
+                WDTCR |= _BV(WDTIE);         // WDT as interrupt
+                asm("sleep");
+                // Wakeup
+                asm("wdr");
+                OUT_ON;
+                _delay_ms(10);
+                OUT_OFF;
+            }
+        }
+    }
+
+    return 0;
+}
 
 ISR(WDT_vect) {
-    ticks_for_quarter = 900 * 20 / wdt8_tc - 1;
+    OUT_OFF;
+    wdt_lock_flag = 0;
 }
 
-// triggered every 0.05 second
-ISR(TIM0_COMPA_vect){
+// Square-blink frequency 10 Hz / trigger freq 20 Hz
+ISR(TIM0_COMPA_vect) {
     ++wdt8_tc;
+    PORTB ^= _BV(OUT_PN);
 }
